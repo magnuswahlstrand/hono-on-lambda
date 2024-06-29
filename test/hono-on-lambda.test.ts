19293c26b18ac9efa6ca35162c6eaf6f72e22b48
magnus <magnus.wahlstrand@gmail.com>
2024-06-29 08:22:38 +0200

    Initial commit

---
+++ test/hono-on-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HonoOnLambda from '../lib/hono-on-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hono-on-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HonoOnLambda.HonoOnLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
